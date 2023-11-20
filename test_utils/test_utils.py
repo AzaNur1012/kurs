b931b86577998c11dd453f3d24194e49fb270209
AzaNur1012 <aznuri@yandex.ru>
2023-11-20 21:37:53 +0500

    add test from filtration and sorted last five operation function

--- test_utils/test_utils.py
+++ test_utils/test_utils.py
@@ -2,14 +2,20 @@ from develop import utils
 
 def test_get_data():
     """тест на функцию чтения json файла """
-    assert len (utils.get_data('operations.json')[0]) > 0
+    assert len(utils.get_data('operations.json')[0]) > 0
 
 
 
 def test_get_operations_executed(test_data):
     """тест на функцию фильтрации операций со статусом EXECUTED"""
-    assert len (utils.get_operations_executed(test_data)) == 2
+    assert len(utils.get_operations_executed(test_data)) == 2
 
+
+
+def test_get_last_num_operations(test_data):
+    """тест на функцию фильтровки и сортировки пяти последних операций"""
+    assert len(utils.get_last_num_operations(test_data, 4)) == 4
+    assert utils.get_last_num_operations(test_data, 4)[0]['date'] == '2023-07-03T18:35:29.512364'
 
 
 
