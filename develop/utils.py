cdf9061ec7072fc5b69e48f1f58b542b466693dc
AzaNur1012 <aznuri@yandex.ru>
2023-11-20 20:35:43 +0500

    add filtred executed operations function

--- develop/utils.py
+++ develop/utils.py
@@ -9,3 +9,17 @@ def get_data(filename):
 
 
 
+def get_operations_executed(data):
+    """функция которая выделяет операции со статусом EXECUTED"""
+    operations_executed = []
+    for operation in data:
+        if 'state' in operation and operation['state'] == 'EXECUTED':
+            operations_executed.append(operation)
+    operation_with_from = []
+    for operation in operations_executed:
+        if 'from' in operation:
+            operation_with_from.append(operation)
+    return operation_with_from
+
+
+
